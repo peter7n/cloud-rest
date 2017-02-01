0993f9a161ba59782a161404d19e01deff175618
Peter Nguyen <ptn045@ucsd.edu>
2017-02-01 10:06:11 -0800

    add delete, patch; add sample-books.txt

--- main.py
+++ main.py
@@ -16,25 +16,48 @@ class Book(ndb.Model):
 class BookHandler(webapp2.RequestHandler):
     def post(self):
         bookData = json.loads(self.request.body)
-        # newBook = Book(title=bookData['title'], isbn=bookData['isbn'], genre=bookData['genre'], author=bookData['author'], checkedIn=bookData['checkedIn'])
         newBook = Book(title=bookData['title'])
-        # newBook.populate(isbn=bookData['isbn'], genre=bookData['genre'], author=bookData['author'], checkedIn=bookData['checkedIn'])
+        # propagate values for newBook
         for bKey, bValue in bookData.iteritems():
             setattr(newBook, bKey, bValue)
         newBook.put()
         bookDict = newBook.to_dict()
-        bookDict['self'] = '/book/' + newBook.key.urlsafe()
+        bookDict['self'] = '/books/' + newBook.key.urlsafe()
         self.response.write(json.dumps(bookDict))
 
-    def get(self, id=None):
-        if id:
-            bookGet = ndb.Key(urlsafe=id).get()
-            bookGetDict = bookGet.to_dict()
-            bookGetDict['self'] = "/book/" + id
-            self.response.write(json.dumps(bookGetDict))
+    def get(self, keyParam=None):
+        if keyParam:
+            bookRetrieve = ndb.Key(urlsafe=keyParam).get()
+            bookRetrieveDict = bookRetrieve.to_dict()
+            bookRetrieveDict['id'] = bookRetrieve.key.id()
+            bookRetrieveDict['self'] = "/books/" + keyParam
+            self.response.write(json.dumps(bookRetrieveDict))
+            # self.response.write(keyParam)
+
+    def delete(self, idParam=None):
+        if idParam:
+            bookObj = Book.get_by_id(int(idParam))
+            bookObj.key.delete()
+            self.response.write("entity deleted")
+
+    def patch(self, idParam=None):
+        if idParam:
+            bookObj = Book.get_by_id(int(idParam))
+            bookData = json.loads(self.request.body)
+            # patch attribute values for bookObj
+            for bKey, bValue in bookData.iteritems():
+                setattr(bookObj, bKey, bValue)
+            bookObj.put()
+            bookDict = bookObj.to_dict()
+            bookDict['self'] = '/books/' + bookObj.key.urlsafe()
+            self.response.write(json.dumps(bookDict))
+
+allowed_methods = webapp2.WSGIApplication.allowed_methods
+new_allowed_methods = allowed_methods.union(('PATCH',))
+webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
     ('/book', BookHandler),
-    ('/book/(.*)', BookHandler),
+    ('/books/(.*)', BookHandler),
 ], debug=True)
