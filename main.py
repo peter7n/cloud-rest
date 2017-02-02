9bc2bd2e66189b82042b4e1cd20f0dc090dacca3
Peter Nguyen <ptn045@ucsd.edu>
2017-02-02 10:40:28 -0800

    add Customer class, extended routes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
 import webapp2
 import json
+import re
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
@@ -12,6 +13,11 @@ class Book(ndb.Model):
     genre = ndb.StringProperty(repeated=True)
     author = ndb.StringProperty()
     checkedIn = ndb.BooleanProperty()
+
+class Customer(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    balance = ndb.FloatProperty()
+    checked_out = ndb.StringProperty(repeated=True)
 
 class BookHandler(webapp2.RequestHandler):
     def post(self):
@@ -32,7 +38,6 @@ class BookHandler(webapp2.RequestHandler):
             bookRetrieveDict['id'] = bookRetrieve.key.id()
             bookRetrieveDict['self'] = "/books/" + keyParam
             self.response.write(json.dumps(bookRetrieveDict))
-            # self.response.write(keyParam)
 
     def delete(self, idParam=None):
         if idParam:
@@ -52,6 +57,51 @@ class BookHandler(webapp2.RequestHandler):
             bookDict['self'] = '/books/' + bookObj.key.urlsafe()
             self.response.write(json.dumps(bookDict))
 
+class CustomerHandler(webapp2.RequestHandler):
+    def post(self):
+        customerData = json.loads(self.request.body)
+        newCustomer = Customer(name=customerData['name'])
+        # propagate values for newCustomer
+        for cKey, cValue in customerData.iteritems():
+            setattr(newCustomer, cKey, cValue)
+        newCustomer.put()
+        customerDict = newCustomer.to_dict()
+        customerDict['self'] = '/customers/' + newCustomer.key.urlsafe()
+        self.response.write(json.dumps(customerDict))
+
+    def get(self, keyParam=None):
+        if keyParam:
+            customerRetrieve = ndb.Key(urlsafe=keyParam).get()
+            customerRetrieveDict = customerRetrieve.to_dict()
+            customerRetrieveDict['id'] = customerRetrieve.key.id()
+            customerRetrieveDict['self'] = "/customers/" + keyParam
+            self.response.write(json.dumps(customerRetrieveDict))
+
+    def delete(self, idParam=None):
+        if idParam:
+            customerObj = Customer.get_by_id(int(idParam))
+            customerObj.key.delete()
+            self.response.write("entity deleted")
+
+    def patch(self, idParam=None):
+        if idParam:
+            customerObj = Customer.get_by_id(int(idParam))
+            customerData = json.loads(self.request.body)
+            # patch attribute values for customerObj
+            for cKey, cValue in customerData.iteritems():
+                setattr(customerObj, cKey, cValue)
+            customerObj.put()
+            customerDict = customerObj.to_dict()
+            customerDict['self'] = '/customers/' + customerObj.key.urlsafe()
+            self.response.write(json.dumps(customerDict))
+
+class CustomerBooksHandler(webapp2.RequestHandler):
+    def get(self, customerId=None):
+        if customerId:
+            self.response.write("success! ")
+            self.response.write(customerId)
+
+# Allow patch operations
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_methods
@@ -60,4 +110,7 @@ app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
     ('/book', BookHandler),
     ('/books/(.*)', BookHandler),
+    ('/customer', CustomerHandler),
+    (r'/customers/(\d+)', CustomerHandler),
+    webapp2.Route(r'/customers/<customerId:\d+>/books', handler=CustomerBooksHandler, name='customer-books'),
 ], debug=True)
