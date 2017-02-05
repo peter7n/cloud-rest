3df1a0c651cc012dd9bf2934e7b0687f7298c216
Peter Nguyen <ptn045@ucsd.edu>
2017-02-04 18:37:49 -0800

    ready, deploy to gcloud

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+###########################################
+# Author: Peter Nguyen
+# Date: 2/5/17
+# CS496-400
+# Description: REST implementation modeling
+# books and customers, using webapp2
+# and Google App Engine ndb
+###########################################
+
 from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
 import webapp2
@@ -14,19 +23,23 @@ class Home(webapp2.RequestHandler):
         listOfCustomerKeys = Customer.query().fetch(keys_only=True)
         ndb.delete_multi(listOfBookKeys)
         ndb.delete_multi(listOfCustomerKeys)
-        self.response.write("All entries successfully delted.")
+        self.response.write("All entries successfully deleted.")
 
 class Book(ndb.Model):
+    b_id = ndb.IntegerProperty()
     title = ndb.StringProperty(required=True)
     isbn = ndb.IntegerProperty()
     genre = ndb.StringProperty(repeated=True)
     author = ndb.StringProperty()
     checkedIn = ndb.BooleanProperty()
+    b_self = ndb.StringProperty()
 
 class Customer(ndb.Model):
+    c_id = ndb.IntegerProperty()
     name = ndb.StringProperty(required=True)
     balance = ndb.FloatProperty()
     checked_out = ndb.StringProperty(repeated=True)
+    c_self = ndb.StringProperty()
 
 class BookHandler(webapp2.RequestHandler):
     def post(self):
@@ -35,19 +48,17 @@ class BookHandler(webapp2.RequestHandler):
         # propagate values for newBook
         for bKey, bValue in bookData.iteritems():
             setattr(newBook, bKey, bValue)
-        newBook.id = newBook.key.id()
-        newBook.self = "/books/" + bookId
+        newBook.put()
+        newBook.b_id = newBook.key.id()
+        newBook.b_self = "/books/" + str(newBook.key.id())
         newBook.put()
         bookDict = newBook.to_dict()
-        # bookDict['self'] = '/books/' + str(newBook.key.id())
         self.response.write(json.dumps(bookDict))
 
     def get(self, bookId=None, queryString=None):
         if bookId:
             bookObj = Book.get_by_id(int(bookId))
             bookObjDict = bookObj.to_dict()
-            # bookObjDict['id'] = bookObj.key.id()
-            # bookObjDict['self'] = "/books/" + bookId
             self.response.write(json.dumps(bookObjDict))
         # Check for a query string value
         qStrVal = self.request.get('checkedIn')
@@ -61,7 +72,7 @@ class BookHandler(webapp2.RequestHandler):
                 query = Book.query(Book.checkedIn == False)
                 for result in query.fetch():
                     checkedOutList.append(result.to_dict())
-            self.response.write(checkedOutList)
+            self.response.write(json.dumps(checkedOutList))
 
     def delete(self, bookId=None):
         if bookId:
@@ -78,7 +89,6 @@ class BookHandler(webapp2.RequestHandler):
                 setattr(bookObj, bKey, bValue)
             bookObj.put()
             bookDict = bookObj.to_dict()
-            bookDict['self'] = '/books/' + str(bookObj.key.id())
             self.response.write(json.dumps(bookDict))
 
 class CustomerHandler(webapp2.RequestHandler):
@@ -89,16 +99,16 @@ class CustomerHandler(webapp2.RequestHandler):
         for cKey, cValue in customerData.iteritems():
             setattr(newCustomer, cKey, cValue)
         newCustomer.put()
+        newCustomer.c_id = newCustomer.key.id()
+        newCustomer.c_self = "/customers/" + str(newCustomer.key.id())
+        newCustomer.put()
         customerDict = newCustomer.to_dict()
-        customerDict['self'] = '/customers/' + str(newCustomer.key.id())
         self.response.write(json.dumps(customerDict))
 
     def get(self, customerId=None):
         if customerId:
             customerObj = Customer.get_by_id(int(customerId))
             customerObjDict = customerObj.to_dict()
-            customerObjDict['id'] = customerObj.key.id()
-            customerObjDict['self'] = "/customers/" + customerId
             self.response.write(json.dumps(customerObjDict))
 
     def delete(self, customerId=None):
@@ -116,7 +126,6 @@ class CustomerHandler(webapp2.RequestHandler):
                 setattr(customerObj, cKey, cValue)
             customerObj.put()
             customerDict = customerObj.to_dict()
-            customerDict['self'] = '/customers/' + str(customerObj.key.id())
             self.response.write(json.dumps(customerDict))
 
 class CustomerBooksHandler(webapp2.RequestHandler):
@@ -127,7 +136,7 @@ class CustomerBooksHandler(webapp2.RequestHandler):
             booksCheckedOut = customerObj.checked_out
             # Fetch book links from checked_out list
             for book in booksCheckedOut:
-                url = "http://localhost:8080" + book
+                url = "http://rest-nguyenp2.appspot.com" + book
                 result = urlfetch.fetch(url)
                 if result.status_code == 200:
                     bookList.append(result.content)
@@ -143,11 +152,10 @@ class CheckoutHandler(webapp2.RequestHandler):
             # Add checked out book to customer's list
             customerObj.checked_out.append("/books/" + str(bookId))
             customerObj.put()
-            #Set book status to checked out
+            # Set book status to checked out
             bookObj.checkedIn = False
             bookObj.put()
             self.response.write(json.dumps(customerObj.to_dict()))
-            self.response.write(bookObj.to_dict())
 
     def delete(self, customerId=None, bookId=None):
         if customerId and bookId:
@@ -156,11 +164,10 @@ class CheckoutHandler(webapp2.RequestHandler):
             # Remove checked out book from customer's list
             customerObj.checked_out.remove("/books/" + str(bookId))
             customerObj.put()
-            #Set book status to checked in
+            # Set book status to checked in
             bookObj.checkedIn = True
             bookObj.put()
-            self.response.write(customerObj.to_dict())
-            self.response.write(bookObj.to_dict())
+            self.response.write(json.dumps(customerObj.to_dict()))
 
 # Allow patch operations
 allowed_methods = webapp2.WSGIApplication.allowed_methods
